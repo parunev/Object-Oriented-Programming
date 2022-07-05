98e82aebc25354f237c44cd41e5e87b2d104ef08
Martin Parunev <parunev@gmail.com>
2022-07-05 15:31:40 +0300

    "Single Inheritance" problem solved

---
+++ src/L03_Inheritance/lab/P01SingleInheritance/Main.java
@@ -0,0 +1,11 @@
+package L03_Inheritance.lab.P01SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.eat();
+        dog.bark();
+    }
+}
