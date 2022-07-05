eee0a7f3f3c4a404d9ea742aac8ddaf2dea656f5
Martin Parunev <parunev@gmail.com>
2022-07-05 15:36:59 +0300

    "Hierarchical Inheritance" problem solved

---
+++ src/L03_Inheritance/lab/P03HierarchicalInheritance/Animal.java
@@ -0,0 +1,7 @@
+package L03_Inheritance.lab.P03HierarchicalInheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/L03_Inheritance/lab/P03HierarchicalInheritance/Cat.java
@@ -0,0 +1,7 @@
+package L03_Inheritance.lab.P03HierarchicalInheritance;
+
+public class Cat extends Animal {
+    public void meow() {
+        System.out.println("meowing...");
+    }
+}
