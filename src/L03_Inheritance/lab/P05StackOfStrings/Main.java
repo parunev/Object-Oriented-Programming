619cc9724b4070a0cf40d7703a49e33e5cbbd104
Martin Parunev <parunev@gmail.com>
2022-07-05 15:46:19 +0300

    "Stack Of Strings" problem solved

---
+++ src/L03_Inheritance/lab/P05StackOfStrings/Main.java
@@ -0,0 +1,17 @@
+package L03_Inheritance.lab.P05StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        StackOfStrings sos = new StackOfStrings();
+        sos.push("one");
+        sos.push("two");
+        sos.push("three");
+
+        System.out.println(sos.isEmpty());
+        System.out.println(sos.peek());
+
+        System.out.println(sos.peek());
+        System.out.println(sos.peek());
+        System.out.println(sos.peek());
+    }
+}
