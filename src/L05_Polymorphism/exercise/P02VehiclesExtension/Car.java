844cdbe2dd1cacfd75bbf8a8c7f7d63b3a1f2ee1
Martin Parunev <parunev@gmail.com>
2022-07-10 23:43:55 +0300

    "Vehicles Extension" problem solved

---
+++ src/L05_Polymorphism/exercise/P02VehiclesExtension/Car.java
@@ -0,0 +1,14 @@
+package L05_Polymorphism.exercise.P02VehiclesExtension;
+
+public class Car extends VehicleImpl {
+    private static final double AC_ADDITIONAL_CONSUMPTION = 0.9;
+
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+    }
+
+    @Override
+    public void setFuelConsumption(double fuelConsumption) {
+        super.setFuelConsumption(fuelConsumption + AC_ADDITIONAL_CONSUMPTION);
+    }
+}
---
+++ src/L05_Polymorphism/exercise/P02VehiclesExtension/Vehicle.java
@@ -0,0 +1,6 @@
+package L05_Polymorphism.exercise.P02VehiclesExtension;
+
+public interface Vehicle {
+    String drive(double distance);
+    void refuel(double liters);
+}
