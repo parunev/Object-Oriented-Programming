453ed0dbd560c7f55bbd46107a0f691e2836dda9
Martin Parunev <parunev@gmail.com>
2022-07-10 21:14:20 +0300

    "Multiple Implementation" problem solved

---
+++ src/L04_Interfaces_and_Abstraction/exercise/P02MultipleImplementation/Birthable.java
@@ -0,0 +1,5 @@
+package L04_Interfaces_and_Abstraction.exercise.P02MultipleImplementation;
+
+public interface Birthable {
+    String getBirthDate();
+}
---
+++ src/L04_Interfaces_and_Abstraction/exercise/P02MultipleImplementation/Person.java
@@ -0,0 +1,6 @@
+package L04_Interfaces_and_Abstraction.exercise.P02MultipleImplementation;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
